a02147e9cf28dc4e02df612e58e99b893dabb5d2
ndelanhese <nnathanh3@gmail.com>
2024-04-14 08:22:53 -0300

    feat(auth): add all permissions types and schemas

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,21 @@
+import type { AbilityBuilder } from '@casl/ability'
+
+import type { AppAbility } from '.'
+import type { User } from './models/user'
+import type { Role } from './roles'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>,
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN: (_, { can }) => {
+    can('manage', 'all')
+  },
+  MEMBER: (user, { can }) => {
+    can(['create', 'get'], 'Project', { ownerId: { $eq: user.id } })
+    can('manage', 'Project')
+  },
+  BILLING: () => {},
+}
