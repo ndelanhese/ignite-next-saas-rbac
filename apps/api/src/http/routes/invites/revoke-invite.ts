f89d59ee3bc9afab0f387fac694daf0f2df60479
ndelanhese <nnathanh3@gmail.com>
2024-05-08 08:30:16 -0300

    feat(invite): add revoke invite

---
+++ apps/api/src/http/routes/invites/revoke-invite.ts
@@ -0,0 +1,63 @@
+import { prisma } from '@lib/prisma'
+import { auth } from '@middlewares/auth'
+import { BadRequestError } from '@routes/_errors/bad-request-error'
+import { UnauthorizedError } from '@routes/_errors/unauthorized-error'
+import { getUserPermissions } from '@utils/get-user-permissions'
+import { type FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export const revokeInvite = async (app: FastifyInstance) => {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:organization_slug/invites/:invite_id',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Revoke an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            organization_slug: z.string(),
+            invite_id: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { organization_slug: slug, invite_id: inviteId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', 'Invite')) {
+          throw new UnauthorizedError("You're not allowed to revoke an invite.")
+        }
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found.')
+        }
+
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
