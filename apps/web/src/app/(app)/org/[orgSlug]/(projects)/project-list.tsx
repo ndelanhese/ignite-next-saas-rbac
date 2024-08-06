fa4a7af803da94fe716ebe8dbc35dd2586a48120
ndelanhese <nnathanh3@gmail.com>
2024-08-06 07:26:41 -0300

    feat: add project list

--- apps/web/src/app/(app)/org/[orgSlug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/org/[orgSlug]/(projects)/project-list.tsx
@@ -1,3 +1,4 @@
+import { getCurrentOrg } from '@auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar'
 import { Button } from '@components/ui/button'
 import {
@@ -7,45 +8,61 @@ import {
   CardHeader,
   CardTitle,
 } from '@components/ui/card'
+import { getProjects } from '@http/get-projects'
 import { getNameInitials } from '@utils/get-name-initals'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { ArrowRight } from 'lucide-react'
 
-export const ProjectList = () => (
-  <div className="grid grid-cols-3 gap-4">
-    <Card>
-      <CardHeader>
-        <CardTitle>Project 01</CardTitle>
-        <CardDescription className="line-clamp-2 leading-relaxed">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          culpa blanditiis autem. Quam voluptates, quas quis repellendus
-          veritatis, nobis et voluptatibus libero vero incidunt optio doloribus,
-          explicabo facilis provident saepe?
-        </CardDescription>
-      </CardHeader>
-      <CardFooter className="flex items-center gap-1.5">
-        <Avatar className="size-4">
-          <AvatarImage src="https://github.com/ndelanhese.png" />
-          <AvatarFallback className="text-[.5rem]">
-            {getNameInitials('Nathan Delanhese')}
-          </AvatarFallback>
-        </Avatar>
+dayjs.extend(relativeTime)
 
-        <span className="text-xs text-muted-foreground">
-          Created by{' '}
-          <span className="font-medium text-foreground">Nathan Delanhese</span>{' '}
-          a day ago
-        </span>
+export const ProjectList = async () => {
+  const currentOrg = getCurrentOrg()
 
-        <Button
-          className="ml-auto"
-          size="xs"
-          variant="outline"
-          icon={<ArrowRight className="ml-2 size-3" />}
-          iconPosition="right"
-        >
-          View
-        </Button>
-      </CardFooter>
-    </Card>
-  </div>
-)
+  const { projects } = await getProjects(currentOrg!)
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {projects.map((project) => (
+        <Card key={project.id} className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle className="text-xl font-medium">
+              {project.name}
+            </CardTitle>
+            <CardDescription className="line-clamp-2 leading-relaxed">
+              {project.description}
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className="flex items-center gap-1.5">
+            <Avatar className="size-4">
+              {project.owner.avatarUrl && (
+                <AvatarImage src={project.owner.avatarUrl} />
+              )}
+              <AvatarFallback className="text-[.5rem]">
+                {getNameInitials(project.owner.name ?? '')}
+              </AvatarFallback>
+            </Avatar>
+
+            <span className="text-xs text-muted-foreground">
+              By{' '}
+              <span className="font-medium text-foreground">
+                {project.owner.name}
+              </span>{' '}
+              {dayjs(project.createdAt).fromNow()}
+            </span>
+
+            <Button
+              className="ml-auto"
+              size="xs"
+              variant="outline"
+              icon={<ArrowRight className="ml-2 size-3" />}
+              iconPosition="right"
+            >
+              View
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  )
+}
